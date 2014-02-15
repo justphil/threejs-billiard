f6abcc828cea6d5c2d9419aed1de51e32b8c692d
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-02-15 21:59:44 +0100

    remove obsolete uncommented portions of old collision response algorithm

--- app/scripts/game/billiard_ball.js
+++ app/scripts/game/billiard_ball.js
@@ -14,18 +14,7 @@
             this.vY     = 3;
 
             this.rotationHelper = Billiard.Helper.RotationHelper;
-
             this.coordsRotationHelper = Billiard.Helper.CoordsRotationHelper;
-
-            /*this.collisionHelper = {
-                ball0Center: new THREE.Vector2(0, 0),
-                ball1Center: new THREE.Vector2(0, 0),
-                v0: new THREE.Vector2(0, 0),
-                v1: new THREE.Vector2(0, 0),
-                n: new THREE.Vector2(0, 0),
-                tmp: new THREE.Vector2(0, 0)
-
-            };*/
 
             // !!! A Billiard.Ball object will be augmented with a mesh property during initialization !!!
         },
@@ -75,75 +64,6 @@
 
             // check for collision based on radius
             if (dist < this.radius + anotherBall.radius) {
-
-
-                /*
-
-                THIS COLLISION HANDLING IS QUITE GOOD BUT BALLS BECOME STUCK!!!
-
-                var angle = Math.atan2(dy, dx),
-                    sin = Math.sin(angle),
-                    cos = Math.cos(angle),
-                    e = 1;
-
-                // rotate velocity vectors
-                var ball0vXr = this.vX * cos + this.vY * sin;
-                var ball0vYr = -this.vX * sin + this.vY * cos;
-
-                var ball1vXr = anotherBall.vX * cos + anotherBall.vY * sin;
-                var ball1vYr = -anotherBall.vX * sin + anotherBall.vY * cos;
-
-                // rotate orientation of line of action
-                // and consider ball0 (pos0) as the reference
-                var pos0 = {x: 0, y: 0};
-                var pos1 = {
-                    x: dx * cos + dy * sin,
-                    y: -dx * sin + dy * cos
-                };
-
-                // apply conservation of momentum
-                var tmp = 1 / (this.mass + anotherBall.mass);
-                var newVxBall0 = (this.mass - e * anotherBall.mass) * ball0vXr * tmp
-                                    + (1 + e) * anotherBall.mass * ball1vXr * tmp;
-
-                var newVxBall1 = (1 + e) * this.mass * ball0vXr * tmp
-                                    + (anotherBall.mass - e * this.mass) * ball1vXr * tmp;
-
-                // update position to avoid objects becoming stuck together
-                var absV = Math.abs(ball0vXr) + Math.abs(ball1vXr),
-                    overlap = (this.radius + anotherBall.radius) - Math.abs(pos0.x - pos1.x);
-                pos0.x += ball0vXr.x / absV * overlap;
-                pos1.x += ball1vXr.x / absV * overlap;
-
-                // rotate positions back
-                var pos0F = {
-                        x: pos0.x * cos - pos0.y * sin,
-                        y: pos0.x * sin + pos0.y * cos
-                    },
-                    pos1F = {
-                        x: pos1.x * cos - pos1.y * sin,
-                        y: pos1.x * sin + pos1.y * cos
-                    };
-
-
-
-                // rotate velocity vectors back
-                this.vX = newVxBall0 * cos - ball0vYr * sin;
-                this.vY = newVxBall0 * sin + ball0vYr * cos;
-
-                anotherBall.vX = newVxBall1 * cos - ball1vYr * sin;
-                anotherBall.vY = newVxBall1 * sin + ball1vYr * cos;
-                */
-
-
-
-
-
-
-
-
-
-
                 var crh = this.coordsRotationHelper;
 
                 // calculate angle, sine, and cosine
@@ -169,7 +89,7 @@
                                     + (anotherBall.mass - e * this.mass) * vel1.x * tmp;
 
 
-                // update position to avoid objects becoming stuck together
+                // update position to avoid that objects become stuck together
                 var absV = Math.abs(newVxBall0) + Math.abs(newVxBall1),
                     overlap = (this.radius + anotherBall.radius) - Math.abs(pos0.x - pos1.x);
                 pos0.x += newVxBall0 / absV * overlap;
