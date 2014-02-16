c22999ee9c6e6cb95a1580a5395da1285216bedf
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-02-16 23:35:19 +0100

    add rotateAroundWorldAxisZ() to rotation helper

--- app/scripts/game/helpers/rotation_helper.js
+++ app/scripts/game/helpers/rotation_helper.js
@@ -2,12 +2,17 @@
     "use strict";
 
     var helperQuaternion        = new THREE.Quaternion(),
+        normalizedZAxisVector   = new THREE.Vector3(0, 0, 1).normalize(),
         normalizedYAxisVector   = new THREE.Vector3(0, 1, 0).normalize(),
         normalizedXAxisVector   = new THREE.Vector3(1, 0, 0).normalize();
 
     function rotateAroundWorldAxis(object, axis, radians) {
         helperQuaternion.setFromAxisAngle(axis, radians); // axis must be normalized, angle in radians
         object.quaternion.multiplyQuaternions(helperQuaternion, object.quaternion);
+    }
+
+    function rotateAroundWorldAxisZ(object, radians) {
+        rotateAroundWorldAxis(object, normalizedZAxisVector, radians);
     }
 
     function rotateAroundWorldAxisY(object, radians) {
@@ -18,6 +23,7 @@
         rotateAroundWorldAxis(object, normalizedXAxisVector, radians);
     }
 
+    Hooray.define('Billiard', 'Helper.RotationHelper', 'rotateAroundWorldAxisZ', rotateAroundWorldAxisZ);
     Hooray.define('Billiard', 'Helper.RotationHelper', 'rotateAroundWorldAxisY', rotateAroundWorldAxisY);
     Hooray.define('Billiard', 'Helper.RotationHelper', 'rotateAroundWorldAxisX', rotateAroundWorldAxisX);
 
