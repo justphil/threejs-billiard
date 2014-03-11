ac629b69ea1bdfeffad96420d763b665cf4e6dc4
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-03-11 19:44:41 +0100

    add collision time sorting in the target guide

--- app/scripts/game/billiard_cue.js
+++ app/scripts/game/billiard_cue.js
@@ -12,9 +12,7 @@
 
             this.distanceTopLeftToBottomRight = 0;
 
-            this.targetGuide = new Billiard.TargetGuide(this.controlRadius);
-
-            this.collisionHelper = Billiard.Helper.CollisionHelper;
+            this.targetGuide = new Billiard.TargetGuide();
 
             /**
              * !!!
@@ -63,39 +61,9 @@
 
                 this.mesh.rotation.z = angle;
 
-                // check if the target guide circle intersects with a ball and determine the point
-
-                var i, n, t, b, endX, endY, ob = this.otherBalls;
-                var fakeVx = this.distanceTopLeftToBottomRight * Math.cos(angle);
-                var fakeVy = this.distanceTopLeftToBottomRight * Math.sin(angle);
-                for (i = 0, n = ob.length; i < n; i++) {
-                    b = ob[i];
-                    // x1, y1, vX1, vY1, r1,
-                    // x2, y2, vX2, vY2, r2
-                    t = this.collisionHelper.getCollisionTime(
-                        ball0Pos.x, ball0Pos.y,
-                        fakeVx,
-                        fakeVy,
-                        this.ball0.radius,
-                        b.mesh.position.x, b.mesh.position.y, 0, 0, b.radius
-                    );
-
-                    if (t !== null && t >= 0 && t <= 1) {
-                        endX = ball0Pos.x + t * fakeVx;
-                        endY = ball0Pos.y + t * fakeVy;
-                        //console.log(endX, endY);
-                        break;
-                    }
-                    else {
-                        endX = ball0Pos.x + (Math.cos(angle) * this.controlRadius);
-                        endY = ball0Pos.y + (Math.sin(angle) * this.controlRadius);
-                    }
-                }
-
+                // angle, ball0, otherBalls, distanceTopLeftToBottomRight, controlRadius
                 this.targetGuide.update(
-                    ball0Pos,
-                    endX,
-                    endY
+                    angle, this.ball0, this.otherBalls, this.distanceTopLeftToBottomRight, this.controlRadius
                 );
             }
             else {
--- app/scripts/game/billiard_target_guide.js
+++ app/scripts/game/billiard_target_guide.js
@@ -2,10 +2,9 @@
     "use strict";
 
     Hooray.defineClass('Billiard', '', 'TargetGuide', {
-        init: function(controlRadius) {
+        init: function() {
             Hooray.log('A new Billiard.TargetGuide instance has been created!');
-
-            this.controlRadius = controlRadius;
+            this.collisionHelper = Billiard.Helper.CollisionHelper;
 
             /**
              * !!!
@@ -20,13 +19,60 @@
             this[prop] = val;
         },
 
-        update: function(ball0Pos, endX, endY) {
+        update: function(angle, ball0, otherBalls, distanceTopLeftToBottomRight, controlRadius) {
+            /**
+             * Basic idea:
+             * Among the other parameters the Cue object passes us the distance from the top-left corner to
+             * the bottom-right corner (main diagonal). This is the longest distance between two points on the playing field.
+             * We use this distance to cast a ray from the white ball in the direction of the current cue orientation.
+             * If there is an intersection between the ray and a ball we take this ball into account for displaying
+             * the target guide circle (TGC) for the frame we are about to calculate.
+             * But we are only interested in the first ball intersection. That's why we sort all ball intersections
+             * by their occurrence time and only consider the first one for the calculation of the TGC's position.
+             */
+
+            var i, n, t, b, endX, endY;
+            var fakeVx = distanceTopLeftToBottomRight * Math.cos(angle);
+            var fakeVy = distanceTopLeftToBottomRight * Math.sin(angle);
+            var ball0Pos = ball0.mesh.position;
+            var timeToConsider = 1;
+            var indexOfRelevantBall = -1;
+
+            for (i = 0, n = otherBalls.length; i < n; i++) {
+                b = otherBalls[i];
+                // Parameters:
+                // x1, y1, vX1, vY1, r1,
+                // x2, y2, vX2, vY2, r2
+                t = this.collisionHelper.getCollisionTime(
+                    ball0Pos.x, ball0Pos.y, fakeVx, fakeVy, ball0.radius,
+                    b.mesh.position.x, b.mesh.position.y, 0, 0, b.radius
+                );
+
+                if (t !== null && t >= 0 && t <= 1) {
+                    if (t < timeToConsider) {
+                        timeToConsider = t;
+                        indexOfRelevantBall = i;
+                    }
+                }
+            }
+
+            if (indexOfRelevantBall !== -1) {
+                endX = ball0Pos.x + timeToConsider * fakeVx;
+                endY = ball0Pos.y + timeToConsider * fakeVy;
+            }
+            else {
+                endX = ball0Pos.x + (Math.cos(angle) * controlRadius);
+                endY = ball0Pos.y + (Math.sin(angle) * controlRadius);
+            }
+
+            // update vertices (dashed line)
             this.mesh.geometry.vertices[0].x = ball0Pos.x;
             this.mesh.geometry.vertices[0].y = ball0Pos.y;
             this.mesh.geometry.vertices[1].x = endX;
             this.mesh.geometry.vertices[1].y = endY;
             this.mesh.geometry.verticesNeedUpdate = true;
 
+            // update TGC
             this.meshCircle.position.x = endX;
             this.meshCircle.position.y = endY;
         }
