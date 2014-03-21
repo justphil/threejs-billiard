d31b0001c4ae165ccaa53f87dc4e2d02518d196d
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-03-21 21:26:50 +0100

    outsource collision reaction code from Ball class to CollisionHelper

--- app/scripts/game/helpers/collision_helper.js
+++ app/scripts/game/helpers/collision_helper.js
@@ -1,6 +1,8 @@
 (function(W, Hooray) {
     "use strict";
 
+    var crh = Billiard.Helper.CoordsRotationHelper;
+
     var helperVectors = {
         p0: new THREE.Vector2(0, 0),
         pf: new THREE.Vector2(0, 0),
@@ -67,5 +69,84 @@
         }
     }
 
+    function calculateBallCollisionReaction(x1, y1, vX1, vY1, r1, m1, x2, y2, vX2, vY2, r2, m2, coefficientOfRestitution) {
+        var returnObj = {};
+
+        var dx = x2 - x1,
+            dy = y2 - y1;
+
+        // calculate angle, sine, and cosine
+        var angle = Math.atan2(dy, dx),
+            sin = Math.sin(angle),
+            cos = Math.cos(angle),
+            e = coefficientOfRestitution,
+        // rotate ball0's velocity
+            vel0 = crh.rotateCoords(vX1, vY1, sin, cos, true),
+        // rotate ball1's velocity
+            vel1 = crh.rotateCoords(vX2, vY2, sin, cos, true);
+
+        // apply conservation of momentum
+        var tmp = 1 / (m1 + m2);
+        var newVxBall0 = (m1 - e * m2) * vel0.x * tmp + (1 + e) * m2 * vel1.x * tmp;
+        var newVxBall1 = (1 + e) * m1 * vel0.x * tmp + (m2 - e * m1) * vel1.x * tmp;
+
+        // apply friction due to oblique collision if it is an oblique collision
+        var obliquenessDetectionVx, obliquenessDetectionVy;
+        if (vel0.x === 0 && vel0.y === 0) {
+            obliquenessDetectionVx = vel1.x;
+            obliquenessDetectionVy = vel1.y;
+        }
+        else {
+            obliquenessDetectionVx = vel0.x;
+            obliquenessDetectionVy = vel0.y;
+        }
+
+        if (isObliqueBallCollision(obliquenessDetectionVx, obliquenessDetectionVy, dx, dy)) {
+            var fiveSeventh = 5 / 7;
+
+            returnObj.vAngularZ1 = -fiveSeventh * (vel0.y / r1);
+            returnObj.vAngularZ2 = fiveSeventh * (vel1.y / r2);
+
+            vel0.y = fiveSeventh * vel0.y;
+            vel1.y = fiveSeventh * vel1.y;
+        }
+        else {
+            returnObj.vAngularZ1 = 0;
+            returnObj.vAngularZ2 = 0;
+        }
+
+        // rotate velocities back
+        var vel0F = crh.rotateCoords(newVxBall0, vel0.y, sin, cos, false),
+            vel1F = crh.rotateCoords(newVxBall1, vel1.y, sin, cos, false);
+
+        returnObj.vX1 = vel0F.x;
+        returnObj.vY1 = vel0F.y;
+        returnObj.vX2 = vel1F.x;
+        returnObj.vY2 = vel1F.y;
+
+        return returnObj;
+    }
+
+    function isObliqueBallCollision(vX0, vY0, vX1, vY1) {
+        // calculate dot product of v0 and v1
+        var dotProduct = vX0 * vX1 + vY0 * vY1;
+
+        // calculate the amount of v0 and v1
+        var amountV0 = Math.sqrt(vX0*vX0 + vY0*vY0);
+        var amountV1 = Math.sqrt(vX1*vX1 + vY1*vY1);
+
+        // calculate cos of the angle between v0 and v1
+        var cosAngle = dotProduct / (amountV0 * amountV1);
+
+        // calculate the angle
+        var angle = Math.acos(cosAngle);
+
+        //console.log('COS ANGLE', angle * 180 / Math.PI, cosAngle);
+
+        // the collision is oblique if the angle is not a multiple of PI/2
+        return (angle % (Math.PI/2)) !== 0;
+    }
+
     Hooray.define('Billiard', 'Helper.CollisionHelper', 'getCollisionTime', getCollisionTime);
+    Hooray.define('Billiard', 'Helper.CollisionHelper', 'calculateBallCollisionReaction', calculateBallCollisionReaction);
 })(window, Hooray);
